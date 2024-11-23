0639dcbaafb8ebb80c8a775170e4397a3e5db732
Gustaw Cyburt <gcyburt@gmail.com>
2024-11-23 09:26:10 +0100

    finished lesson 12

--- lesson-12/app.ts
+++ lesson-12/app.ts
@@ -4,9 +4,12 @@ import fs from 'fs';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
-const loadTextFiles = (folderPath: string): string[] => {
+const loadTextFiles = (folderPath: string): { content: string, filename: string }[] => {
     const files = fs.readdirSync(folderPath);
-    return files.map(file => fs.readFileSync(path.join(folderPath, file), 'utf-8'));
+    return files.map(file => ({
+        content: fs.readFileSync(path.join(folderPath, file), 'utf-8'),
+        filename: file
+    }));
 };
 
 const qdrantClient = new QdrantClient({
@@ -18,29 +21,33 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-await qdrantClient.createCollection('ai-devs-3', {
-    vectors: {
-        size: 1536,
-        distance: 'Cosine',
-    },
-});
-
-const result = await qdrantClient.getCollections();
-console.log(result);
-
 // Main function to process and send data to Qdrant
 async function processAndSendToQdrant() {
+    try {
+        await qdrantClient.createCollection('ai-devs-3', {
+            vectors: {
+                size: 1536,
+                distance: 'Cosine',
+            },
+        });
+    } catch (error) {
+        console.log("Collection already exists");
+        return;
+    }
+
     const folderPath = './lesson-12/data'; // Path to your data folder
     const texts = loadTextFiles(folderPath);
 
     for (const text of texts) {
-        const embedding = await getEmbedding(text);
+        const embedding = await getEmbedding(text.content);
+
         await qdrantClient.upsert('ai-devs-3', {
             points: [{
                 id: uuidv4(),
                 vector: embedding,
                 payload: {
-                    content: 'chunk'
+                    date: text.filename,
+                    content: text.content
                 }
             }]
         });
@@ -55,4 +62,16 @@ async function getEmbedding(prompt: string): Promise<number[]> {
     return response.data[0].embedding;
 }
 
+async function searchForWeapon(weapon: string) {
+    const embedding = await getEmbedding(weapon);
+    const result = await qdrantClient.search('ai-devs-3', {
+        vector: embedding,
+        limit: 3
+    });
+    return result;
+}
+
 processAndSendToQdrant().catch(console.error);
+
+const result = await searchForWeapon('W raporcie, z którego dnia znajduje się wzmianka o kradzieży prototypu broni?');
+console.log(result);
